377e5adea69a415fe6d7253a80d197d0867dd1b4
Will Crawford <will@metawhimsy.com>
2010-09-02 15:27:30 -0700

    Problem 20 solved

--- Problems020-029/problem20.py
+++ Problems020-029/problem20.py
@@ -6,12 +6,17 @@ problem20.py
 Created by Whimsy on 2010-08-29.
 Copyright (c) 2010 Will Crawford. All rights reserved.
 
-This program sums the digits of 100 factorial.
+This program sums the digits of factorial(100).
 """
 
 import sys
 import os
 from math import factorial # Is this cheating?
+
+def _factorial(n): # Just to prove I can do it.
+	if n == 1: return 1
+	else: return _factorial(n-1) * n
+	# Factorials are very, simple, obvious use-cases for recursion.
 
 def main():
 	a = list(str(factorial(100)))
@@ -21,7 +26,5 @@ def main():
 	print str(sum)
 	return 0
 
-
 if __name__ == '__main__':
 	main()
-
